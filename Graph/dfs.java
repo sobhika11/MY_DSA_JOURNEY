f5bfa8dd1ce73fa4ffa6a5e909d1506b98d4987a
sobhika11 <sobhika1105@gmail.com>
2025-12-20 11:18:46 +0530

    Added dfs solution

---
+++ Graph/dfs.java
@@ -0,0 +1,20 @@
+class Solution {
+    ArrayList<Integer>ans=new ArrayList<>();
+    public void dfs(boolean vis[],int i,ArrayList<ArrayList<Integer>> adj){
+        vis[i]=true;
+        ans.add(i);
+        for(int v:adj.get(i))
+        {
+            if(!vis[v])
+                dfs(vis,v,adj);
+        }
+        
+    }
+    public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        boolean vis[]=new boolean[adj.size()];
+        dfs(vis,0,adj);
+        return ans;
+        
+    }
+}
