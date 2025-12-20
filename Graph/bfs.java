e76b93935c3e9cac4082b88bd0a36cb5e89498e2
sobhika11 <sobhika1105@gmail.com>
2025-12-20 11:17:53 +0530

    Added bfs solution

---
+++ Graph/bfs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+        int [] visited=new int[adj.size()];
+        Queue<Integer>q=new LinkedList<>();
+        q.add(0);
+        visited[0]=1;
+        while(!q.isEmpty()){
+            int ch=q.poll();
+            ans.add(ch);
+            for(int c:adj.get(ch))
+                {
+                    if(visited[c]!=1)
+                        q.add(c);
+                        visited[c]=1;
+                }
+        }
+        return ans;
+        
+    }
+}
