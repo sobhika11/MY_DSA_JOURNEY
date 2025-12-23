3c4aa3e58e0af29357661383bfa58ba30deb5a29
sobhika11 <sobhika1105@gmail.com>
2025-12-23 12:06:07 +0530

    topo sort solution

--- Graph/topo sort.java
+++ Graph/topo sort.java
@@ -1,34 +1,28 @@
 class Solution {
-    ArrayList<Integer>li=new ArrayList<>();
-    public void bfs(ArrayList<ArrayList<Integer>>adj,int []deg){
-        Queue<Integer>q=new LinkedList<>();
-        for(int i=0;i<deg.length;i++){
-            if(deg[i]==0)
-                q.add(i);
+    int i=0;
+    public void dfs(ArrayList<ArrayList<Integer>>adj,ArrayList<Integer>li,boolean vis [],int nd){
+        vis[nd]=true;
+        for(int val:adj.get(nd)){
+            if(!vis[val])
+                dfs(adj,li,vis,val);
         }
-        while(!q.isEmpty()){
-            int val=q.poll();
-            li.add(val);
-            for(int v:adj.get(val)){
-                deg[v]--;
-                if(deg[v]==0)
-                    q.add(v);
-            }
-        }
-        
+        li.set(i--,nd);
     }
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
         // code here
+        this.i=V-1;
         ArrayList<ArrayList<Integer>>adj=new ArrayList<>(V);
-        int deg[]=new int[V];
+        ArrayList<Integer>li=new ArrayList<>(V);
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
+            li.add(0);
         }
-        for(int e[]:edges){
+        for(int e[]:edges)
             adj.get(e[0]).add(e[1]);
-            deg[e[1]]++;
-        }
-        bfs(adj,deg);
+        boolean vis[]=new boolean[V];
+        for(int nd=0;nd<V;nd++){
+            if(!vis[nd])
+                dfs(adj,li,vis,nd);}
         return li;
         
     }
