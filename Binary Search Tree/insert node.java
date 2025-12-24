89ab9c82bb42540603e83fbe3986b845c3cc97d1
sobhika11 <sobhika1105@gmail.com>
2025-12-24 09:19:56 +0530

    insert node solution

--- Binary Search Tree/insert node.java
+++ Binary Search Tree/insert node.java
@@ -1,5 +1,29 @@
-package Binary Search Tree;
-
-public class insert node {
-    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertNodeUtil(TreeNode root,int x){
+        if(root==null)
+            return new TreeNode(x);
+        if(x>root.val)
+            root.right=insertNodeUtil(root.right,x);
+        else
+            root.left=insertNodeUtil(root.left,x);
+        return root;
+    }
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        return insertNodeUtil(root,val);
+    }
 }
