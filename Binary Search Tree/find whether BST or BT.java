4bbad53a769c3814d5efc2d0b66fe6436158c640
sobhika11 <sobhika1105@gmail.com>
2025-12-24 09:21:55 +0530

    Bst or BT validation solution

--- Binary Search Tree/find whether BST or BT.java
+++ Binary Search Tree/find whether BST or BT.java
@@ -1,5 +1,27 @@
-package Binary Search Tree;
-
-public class find whether BST or BT {
-    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    long x=Long.MIN_VALUE,y=Long.MAX_VALUE;
+    public boolean dfs(TreeNode root,long y,long x){
+        if(root==null)
+            return true;
+        return root.val>x && root.val<y && (dfs(root.left,root.val,x) && dfs(root.right,y,root.val));
+        
+    }
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root,y,x);
+    }
 }
