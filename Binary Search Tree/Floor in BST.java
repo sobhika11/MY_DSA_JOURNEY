ffebd56fd7837fabbcb50d6b472691dfaa9e863c
sobhika11 <sobhika1105@gmail.com>
2025-12-25 23:20:40 +0530

    floor in BST soln

--- Binary Search Tree/Floor in BST.java
+++ Binary Search Tree/Floor in BST.java
@@ -1,3 +1,28 @@
-public class Floor in BST {
-    
+/* class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    int floor(Node root, int x) {
+        // code here
+        int ceil=-1;
+        while(root!=null){
+            if(root.data==x)
+                return root.data;
+            if(x>root.data)
+            {
+                ceil=root.data;
+                root=root.right;
+            }
+            else
+                root=root.left;
+        }
+    return ceil;
+    }
 }
