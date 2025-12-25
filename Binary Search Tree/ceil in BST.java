8b64c8098a861da47ce390abdddd8c19ba952e69
sobhika11 <sobhika1105@gmail.com>
2025-12-25 23:18:59 +0530

    ceil in BST soln

--- Binary Search Tree/ceil in BST.java
+++ Binary Search Tree/ceil in BST.java
@@ -1,3 +1,28 @@
-public class ceil in BST {
-    
+/* class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    int findCeil(Node root, int x) {
+        // code here
+        int ceil=-1;
+        while(root!=null){
+            if(root.data==x)
+                return root.data;
+            if(x<root.data)
+            {
+                ceil=root.data;
+                root=root.left;
+            }
+            else
+                root=root.right;
+        }
+    return ceil;
+    }
 }
