c90c76fc7ffed0bb7aadab5d0b428e2c0755537f
sobhika11 <sobhika1105@gmail.com>
2025-12-26 23:54:43 +0530

    Majority elements soln

--- Array/Majority elements.java
+++ Array/Majority elements.java
@@ -1,3 +1,10 @@
 public class Majority elements {
     
 }
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length/2;
+    return nums[n];
+    }
+}
