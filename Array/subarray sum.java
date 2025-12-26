939b056de4c889fb892c4f1651536e610c2a92a1
sobhika11 <sobhika1105@gmail.com>
2025-12-26 23:47:24 +0530

    subarray sum solution

--- Array/subarray sum.java
+++ Array/subarray sum.java
@@ -1,3 +1,15 @@
-public class subarray sum {
-    
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int s=0,c=0;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        map.put(0,1);
+        for(int i:nums)
+        {
+            s+=i;
+            if(map.containsKey(s-k))
+                c+=map.get(s-k);
+            map.put(s,map.getOrDefault(s,0)+1);
+        }
+        return c;
+    }
 }
