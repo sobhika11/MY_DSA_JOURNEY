4ce5b2462aaf58a2bd36accab802bc51c5513513
sobhika11 <sobhika1105@gmail.com>
2025-12-28 22:06:06 +0530

    Odd Even Linked List soln

--- LinkedList/Odd Even Linked List.java
+++ LinkedList/Odd Even Linked List.java
@@ -10,6 +10,42 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        
+        ListNode ehead=null,etail=null;
+        ListNode ohead=null,otail=null;
+        int c=1;
+        while(head!=null)
+            {
+                ListNode nd=new ListNode(head.val);
+                if(c%2==1)
+                {
+                    if(ohead==null)
+                    {
+                        ohead=nd;
+                        otail=nd;
+                    }
+                    else{
+                        otail.next=nd;
+                        otail=nd;
+                    }
+                }
+                else
+                {
+                    if(ehead==null)
+                    {
+                        ehead=nd;
+                        etail=nd;
+                    }
+                    else{
+                        etail.next=nd;
+                        etail=nd;
+                    
+                    }
+                }
+            head=head.next;
+            c++;
+            }
+        if(ehead!=null)
+            otail.next=ehead;
+        return ohead;
     }
 }
