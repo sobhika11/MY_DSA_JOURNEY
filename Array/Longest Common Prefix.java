4e328bd9f6ea13d19c4223826a6162c2c1e966f1
sobhika11 <sobhika1105@gmail.com>
2026-01-01 08:25:27 +0530

    Median of Two Sorted Arrays soln

--- Array/Longest Common Prefix.java
+++ Array/Longest Common Prefix.java
@@ -1,3 +1,18 @@
-public class Longest Common Prefix {
-    
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        int n=strs.length,min=Integer.MAX_VALUE;
+        char [] a=strs[0].toCharArray();
+        char [] b=strs[n-1].toCharArray();
+        for(int i=0;i<n;i++)
+            min=Math.min(min,strs[i].length());
+        String s="";
+        for(int i=0;i<min;i++)
+        {
+            if(a[i]!=b[i])
+                break;
+            s+=a[i];
+        }
+    return s;
+    }
 }
