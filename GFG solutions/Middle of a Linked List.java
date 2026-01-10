d028499e7ce1793ade46be13158b07c83b749f6b
sobhika11 <sobhika1105@gmail.com>
2026-01-10 10:28:51 +0530

    Detect loop in Linkedlist

--- GFG solutions/Middle of a Linked List.java
+++ GFG solutions/Middle of a Linked List.java
@@ -1,3 +1,23 @@
-public class Middle of a Linked List {
-    
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+} */
+
+class Solution {
+    int getMiddle(Node head) {
+        // code here
+        Node fast=head,slow=head;
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        return slow.data;
+    }
 }
