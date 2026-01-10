a37fbf19a2f613ffe2df2deff38f6592c8a1b189
sobhika11 <sobhika1105@gmail.com>
2026-01-10 10:30:40 +0530

    Detect loop in Linkedlist

--- GFG solutions/Reverse a linked list.java
+++ GFG solutions/Reverse a linked list.java
@@ -1,3 +1,26 @@
-public class Reverse a linked list {
-    
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        Node prev = null, curr = head, next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+        
+    }
 }
