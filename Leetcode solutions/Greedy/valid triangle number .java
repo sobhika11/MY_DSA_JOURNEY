6c2e6f27faefc5b9ad4b072e00d33e92a3481c4c
Sobhika <sobhika1105@gmail.com>
2026-01-26 10:14:18 +0530

    valid triangle number solution

---
+++ Leetcode solutions/Greedy/valid triangle number .java
@@ -0,0 +1,19 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int c=0;
+        for(int k=nums.length-1;k>=2;k--)
+            {
+                int i=0,j=k-1;
+                while(i<j)
+                    {
+                        if(nums[i]+nums[j]>nums[k]){
+                            c+=j-i;
+                            j--;}
+                        else
+                            i++;
+                    }
+            }
+        return c;
+    }
+}
