963c896fed92b2365c8171c2519248280b36df10
Sobhika <sobhika1105@gmail.com>
2026-02-03 22:19:30 +0530

    String atsk solution

--- Codeforces solutions/String task.java
+++ Codeforces solutions/String task.java
@@ -4,13 +4,18 @@ public class Main
 	public static void main(String[] args) {
 	Scanner read=new Scanner(System.in);
 	String s=read.nextLine();
-	ArrayList<Character>li=new ArrayList<>(Arrays.asList('a','e','i','o','u'));
+	ArrayList<Character>li=new ArrayList<>(Arrays.asList('a','y','e','i','o','u'));
 	s=s.toLowerCase();
 	StringBuilder sb=new StringBuilder();
 	for(char ch:s.toCharArray()){
-	    if(!li.contains(ch)){
+	    if(li.contains(ch))
+	      continue;
+	    else if(Character.isLetter(ch)){
 	        sb.append('.');
-	        sb.append(ch);}
+	      sb.append(ch);
+	    }
+	    else if (ch == ' ')
+    sb.append(' ');
 	}
 	System.out.println(sb.toString());
 	
