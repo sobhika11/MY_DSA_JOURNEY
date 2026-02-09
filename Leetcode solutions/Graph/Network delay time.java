09b0802e63ab81b369fcb097c9e2708d051254e6
Sobhika <sobhika1105@gmail.com>
2026-02-09 15:06:24 +0530

    Network delay time solution

--- Leetcode solutions/Graph/Network delay time.java
+++ Leetcode solutions/Graph/Network delay time.java
@@ -1,3 +1,34 @@
-public class Network delay time {
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        ArrayList<ArrayList<int []>>adj=new ArrayList<>();
+        for(int i=0;i<=n;i++)
+            adj.add(new ArrayList<>());
+        for(int e[]:times)
+            adj.get(e[0]).add(new int[]{e[1],e[2]});
+        PriorityQueue<int []>q=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        int dist[]=new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[k]=0;
+        q.add(new int[]{k,0});
+        while(!q.isEmpty()){
+            int a[]=q.poll();
+            int d=a[1];
+            int nd=a[0];
+            for(int nei[]:adj.get(nd)){
+                int ndis=nei[1]+d;
+                if(dist[nei[0]]>ndis){
+                    dist[nei[0]]=ndis;
+                    q.add(new int[]{nei[0],ndis});
+                }
+            }
+        }
+    int max=0;
+    for(int i=1;i<=n;i++){
+    if(dist[i]==Integer.MAX_VALUE)  
+        return -1;
+    max=Math.max(dist[i],max);
+    }
+    return max;
+    }
     
 }
