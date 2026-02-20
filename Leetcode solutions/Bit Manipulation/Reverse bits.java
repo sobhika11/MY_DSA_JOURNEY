c5acec10f0d8b66ea0aac199591cf61354398663
Sobhika <sobhika1105@gmail.com>
2026-02-20 10:55:08 +0530

    reverse bits soln

--- Leetcode solutions/Bit Manipulation/Reverse bits.java
+++ Leetcode solutions/Bit Manipulation/Reverse bits.java
@@ -1,5 +1,11 @@
-package Leetcode solutions.Bit Manipulation;
-
-public d {
-    
+class Solution {
+    public int reverseBits(int n) {
+        int res=0,c=0;
+        for(int i=0;i<32;i++){
+            int b=n%2;
+            res=res*2+b;
+            n/=2;
+        }
+        return res;
+    }
 }
