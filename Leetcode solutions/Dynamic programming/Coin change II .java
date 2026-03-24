bbdf63395fefb92aee8c22ffb1382ebeb5869d89
Sobhika <sobhika1105@gmail.com>
2026-03-24 14:35:12 +0530

    coin change II soln

---
+++ Leetcode solutions/Dynamic programming/Coin change II .java
@@ -0,0 +1,21 @@
+class Solution {
+    public int coins(int amt,int [] coins,int i,int [][]dp){
+        if(amt==0)
+            return 1;
+        if(amt<0)
+            return 0;
+        if(dp[i][amt]!=-1)  return dp[i][amt];
+        int ans=0;
+        for(int j=i;j<coins.length;j++)
+            ans+=coins(amt-coins[j],coins,j,dp);
+        return dp[i][amt]=ans;
+    }
+    public int change(int amount, int[] coins) {
+        // return coins(amount,coins,0);
+        int n=coins.length;
+        int dp[][]=new int[n][amount+1];
+        for(int i=0;i<n;i++)
+            Arrays.fill(dp[i],-1);
+        return coins(amount,coins,0,dp);
+    }
+}
