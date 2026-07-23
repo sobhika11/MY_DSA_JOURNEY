575406712e7103c9b522cd819dbf2ff95b5f0c09
Sobhika <sobhika1105@gmail.com>
2026-07-23 23:50:02 +0530

    Selection sort question

---
+++ GFG solutions/Selection sort.java
@@ -0,0 +1,15 @@
+class Solution {
+    void selectionSort(int[] arr) {
+        // code here
+        for(int i=0;i<arr.length-1;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min])
+                    min=j;
+            }
+            int t=arr[i];
+            arr[i]=arr[min];
+            arr[min]=t;
+        }
+    }
+}
